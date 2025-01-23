cbad30f1b480e4691d188c2feecd4184555be162
Kim, Sung Yeon <kimjoans@gmail.com>
2025-01-23 19:49:25 +0900

    first commit

---
+++ src/test/java/net/scit/myboard/MyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package net.scit.myboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
